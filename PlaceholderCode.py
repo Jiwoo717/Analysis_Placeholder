6c73b88aa2eb7f9edd3357f1fe67f5971d9434fe
Eric <89174461+Jiwoo717@users.noreply.github.com>
2023-09-16 17:33:52 -0700

    Update PlaceholderCode.py

--- PlaceholderCode.py
+++ PlaceholderCode.py
@@ -8,3 +8,11 @@ API_KEY = os.environ.get('api')
 API_SECRET_KEY = os.environ.get('secret_key')
 ACCESS_TOKEN = os.environ.get('token')
 ACCESS_TOKEN_SECRET = os.environ.get('secret_token')
+
+def setup_api():
+    """ Setup the tweepy API with authentication. """
+    auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    return api
